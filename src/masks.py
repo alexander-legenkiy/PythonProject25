b9db009131f77b38f60729c8966382bd073314d4
Alexander Legenkiy <alexander-legenkiy@yandex.ru>
2026-04-23 11:26:26 +0300

    commit №1

---
+++ src/masks.py
@@ -0,0 +1,26 @@
+def get_mask_card_number(card_number: str) -> str:
+    """функция принимает на вход номер карты и возвращает его маску"""
+    mask_card_number = str(
+        (card_number[0:4]) + " " + (card_number[4:6]) + "**" + " " + "****" + " " + (card_number[12:16])
+    )
+    # переводм номер карты в заданный формат
+    return mask_card_number
+    # функция возвращает номер счёта в виде заданного формата
+
+
+# card_number = input("Введите номер Вашей банковской карты:")
+# # проверка работы функции
+# print(get_mask_card_number(card_number))
+
+
+def get_mask_account(account_number: str) -> str:
+    """функций принимает на вход номер счёта и возвращает его маску"""
+    mask_account_number = str("**" + (account_number[-4:]))
+    # переводм номер карты в заданный формат
+    return mask_account_number
+    # функция возвращает номер счёта в виде заданного формата
+
+
+# # проверка работы функции
+# account_number = input("Введите номер Вашего банковского счёта:")
+# print(get_mask_account(account_number))
