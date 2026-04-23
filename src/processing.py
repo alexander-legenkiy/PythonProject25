dbabe5a340bf3826ec44c0f029c310d417f03bc7
alexander-legenkiy <alexander.legenkiy@gmail.com>
2026-04-23 17:29:24 +0300

    file after black and isort deployment

--- src/processing.py
+++ src/processing.py
@@ -17,10 +17,12 @@ def filter_by_state(dictionary_list: list, state="EXECUTED") -> list:
 # проверка работы функции
 
 
-dictionary_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                   {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+dictionary_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 print(filter_by_state(dictionary_list, "EXECUTED"))
 
 
@@ -28,15 +30,18 @@ def sort_by_date(dictionary_list: list, reverse=False) -> list:
     # """функция принимает список словарей и возвращает новый список отсортированный по дате,
     # по умолчанию отсортированный по убыванию"""
 
-    sorted_new_dictionary_list = sorted(dictionary_list, key=lambda x: x['date'], reverse=reverse)
+    sorted_new_dictionary_list = sorted(dictionary_list, key=lambda x: x["date"], reverse=reverse)
     return sorted_new_dictionary_list
     #  функция возвращает новый список словарей
+
 
 # проверка работы функции
 
 
-dictionary_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                   {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+dictionary_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 print(sort_by_date(dictionary_list))
